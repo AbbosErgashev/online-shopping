beaeace290a5588ff6ca5dd053e96516436b3527
eaxusniddinovich <eaxusniddinovich@gmail.com>
2025-04-22 22:25:39 +0500

    develop: finished

---
+++ app/src/main/java/uz/online/onlineshop/Fragment/ReviewFragment.java
@@ -0,0 +1,38 @@
+package uz.online.onlineshop.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uz.online.onlineshop.R;
+
+public class DescriptionFragment extends Fragment {
+    public DescriptionFragment() {
+
+    }
+
+    public static DescriptionFragment newInstance(String param1, String param2) {
+        DescriptionFragment fragment = new DescriptionFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description, container, false);
+    }
+}
---
+++ app/src/main/java/uz/online/onlineshop/Fragment/SoldFragment.java
@@ -0,0 +1,37 @@
+package uz.online.onlineshop.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import uz.online.onlineshop.R;
+
+public class ReviewFragment extends Fragment {
+    public ReviewFragment() {
+
+    }
+
+    public static ReviewFragment newInstance(String param1, String param2) {
+        ReviewFragment fragment = new ReviewFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description, container, false);
+    }
+}
